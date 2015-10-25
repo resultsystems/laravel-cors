d73a7d2ea8fbdb305792b59b7ba3806607659cf9
emtudo <emtudo@gmail.com>
2015-10-25 15:13:04 -0200

    Middleware refactoring

--- src/CorsMiddleware.php
+++ src/CorsMiddleware.php
@@ -17,21 +17,30 @@ class CorsMiddleware implements Middleware
      */
     public function handle($request, Closure $next)
     {
-        $allow = '';
+        $allow = $this->getPermission();
 
+        return $next($request)->header('Access-Control-Allow-Origin', $allow)
+            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Origin');
+    }
+
+    public function getPermission()
+    {
+        $allow = '';
         $permissions = Config::get('cors.permissions', ['*']);
+
+        if ($permissions == '*' || ['*']) {
+            return '*';
+        }
 
         if (isset($_SERVER['HTTP_REFERER'])) {
             $url = parse_url($_SERVER['HTTP_REFERER']);
 
             if (in_array($url['host'], $permissions)) {
-                $allow = $url['scheme'].'://'.$url['host'];
-                //$allow = '*';
+                return $url['scheme'].'://'.$url['host'];
             }
         }
 
-        return $next($request)->header('Access-Control-Allow-Origin', $allow)
-            ->header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE')
-            ->header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization, X-Requested-With, Origin');
+        return '';
     }
 }
